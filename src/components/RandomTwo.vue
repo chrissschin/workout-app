3f009d980c149607019a71fe6bc6ab34b4903554
Christian Solis <solischris92@gmail.com>
2019-04-25 16:01:03 -0700

    added a random component

---
+++ src/components/RandomTwo.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>random template</h1>
+</template>
+
+<script>
+export default {
+    name: 'RnadomTwo',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style></style>
+
+
